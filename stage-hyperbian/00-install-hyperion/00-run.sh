d73d0588ab8a3756c08d4c7bc081a669a58b4b7d
tihoangyeudau <tuantrung2905@gmail.com>
2020-08-02 21:35:29 +0700

    Update 00-run.sh

--- stage-hyperbian/00-install-hyperion/00-run.sh
+++ stage-hyperbian/00-install-hyperion/00-run.sh
@@ -1,21 +1,21 @@
 #!/bin/bash -e
 
 # Select the appropriate download path
-HYPERION_DOWNLOAD_URL="https://github.com/hyperion-project/hyperion.ng/releases/download"
-HYPERION_RELEASES_URL="https://api.github.com/repos/hyperion-project/hyperion.ng/releases"
+HYPERION_DOWNLOAD_URL="https://github.com/tihoangyeudau/hyperion.ng-1/releases/download"
+HYPERION_RELEASES_URL="https://api.github.com/repos/tihoangyeudau/hyperion.ng-1/releases"
 
 # Get the latest version
 HYPERION_LATEST_VERSION=$(curl -sL "$HYPERION_RELEASES_URL" | grep "tag_name" | head -1 | cut -d '"' -f 4)
-HYPERION_RELEASE=$HYPERION_DOWNLOAD_URL/$HYPERION_LATEST_VERSION/Hyperion-$HYPERION_LATEST_VERSION-Linux-armv6l.tar.gz
+HYPERION_RELEASE=$HYPERION_DOWNLOAD_URL/$HYPERION_LATEST_VERSION/Ambilight-WiFi-$HYPERION_LATEST_VERSION-Linux-armv6l.tar.gz
 
 # Download latest release
-echo '           Download Hyperion'
-curl -sS -L --get $HYPERION_RELEASE | tar --strip-components=1 -C ${ROOTFS_DIR}/usr/share/ share/hyperion -xz
+echo '           Download Ambilight WiFi'
+curl -sS -L --get $HYPERION_RELEASE | tar --strip-components=1 -C ${ROOTFS_DIR}/usr/share/ share/ambilightwifi -xz
 
 # Copy service file and cleanup
-cp hyperion.service ${ROOTFS_DIR}/etc/systemd/system/hyperion.service
-rm -r ${ROOTFS_DIR}/usr/share/hyperion/service
-rm -r ${ROOTFS_DIR}/usr/share/hyperion/desktop 2>/dev/null
+cp ambilightwifi.service ${ROOTFS_DIR}/etc/systemd/system/ambilightwifi.service
+rm -r ${ROOTFS_DIR}/usr/share/ambilightwifi/service
+rm -r ${ROOTFS_DIR}/usr/share/ambilightwifi/desktop 2>/dev/null
 
 # Enable SPI and force HDMI output
 sed -i "s/^#dtparam=spi=on.*/dtparam=spi=on/" ${ROOTFS_DIR}/boot/config.txt
@@ -25,19 +25,16 @@ sed -i "s/^#hdmi_force_hotplug=1.*/hdmi_force_hotplug=1/" ${ROOTFS_DIR}/boot/con
 sed -i "s/Raspbian/HyperBian/gI" ${ROOTFS_DIR}/usr/lib/os-release
 sed -i "s/^NAME=.*$/NAME=\"HyperBian ${HYPERION_LATEST_VERSION}\"/g" ${ROOTFS_DIR}/usr/lib/os-release
 sed -i "s/^VERSION=.*$/VERSION=\"${HYPERION_LATEST_VERSION}\"/g" ${ROOTFS_DIR}/usr/lib/os-release
-sed -i "s/^HOME_URL=.*$/HOME_URL=\"https:\/\/www.hyperion-project.org\/\"/g" ${ROOTFS_DIR}/usr/lib/os-release
-sed -i "s/^SUPPORT_URL=.*$/SUPPORT_URL=\"https:\/\/forum.hyperion-project.org\/\"/g" ${ROOTFS_DIR}/usr/lib/os-release
-sed -i "s/^BUG_REPORT_URL=.*$/BUG_REPORT_URL=\"https:\/\/forum.hyperion-project.org\/\"/g" ${ROOTFS_DIR}/usr/lib/os-release
 
 on_chroot << EOF
-echo '           Install Hyperion'
-chmod +x -R /usr/share/hyperion/bin
-ln -fs /usr/share/hyperion/bin/hyperiond /usr/bin/hyperiond
-ln -fs /usr/share/hyperion/bin/hyperion-remote /usr/bin/hyperion-remote
-ln -fs /usr/share/hyperion/bin/hyperion-v4l2 /usr/bin/hyperion-v4l2
-ln -fs /usr/share/hyperion/bin/hyperion-framebuffer /usr/bin/hyperion-framebuffer 2>/dev/null
-ln -fs /usr/share/hyperion/bin/hyperion-dispmanx /usr/bin/hyperion-dispmanx 2>/dev/null
-ln -fs /usr/share/hyperion/bin/hyperion-qt /usr/bin/hyperion-qt 2>/dev/null
-echo '           Register Hyperion'
-systemctl -q enable hyperion.service
+echo '           Install Ambilight WiFi'
+chmod +x -R /usr/share/ambilightwifi/bin
+ln -fs /usr/share/ambilightwifi/bin/ambilightwifid /usr/bin/ambilightwifid
+ln -fs /usr/share/ambilightwifi/bin/ambilightwifi-remote /usr/bin/ambilightwifi-remote
+ln -fs /usr/share/ambilightwifi/bin/ambilightwifi-v4l2 /usr/bin/ambilightwifi-v4l2
+ln -fs /usr/share/ambilightwifi/bin/ambilightwifi-framebuffer /usr/bin/ambilightwifi-framebuffer 2>/dev/null
+ln -fs /usr/share/ambilightwifi/bin/ambilightwifi-dispmanx /usr/bin/ambilightwifi-dispmanx 2>/dev/null
+ln -fs /usr/share/ambilightwifi/bin/ambilightwifi-qt /usr/bin/ambilightwifi-qt 2>/dev/null
+echo '           Register Ambilight WiFi'
+systemctl -q enable ambilightwifi.service
 EOF
