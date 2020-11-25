e36dc6a953873c382a54c8a04e5b57c0e0989390
tihoangyeudau <tuantrung2905@gmail.com>
2020-11-26 01:09:21 +0700

    Update 00-run.sh

--- stage-hyperbian/00-install-hyperion/00-run.sh
+++ stage-hyperbian/00-install-hyperion/00-run.sh
@@ -11,7 +11,7 @@ HYPERION_RELEASE=$HYPERION_DOWNLOAD_URL/$HYPERION_LATEST_VERSION/Ambilight-WiFi-
 # Download latest release
 echo '           Download Ambilight WiFi + rpi fan'
 mkdir -p "$ROOTFS_DIR"/tmp
-curl -L $HYPERION_RELEASE --output "$ROOTFS_DIR"/tmp/ambilightwifi.deb
+curl -L $HYPERION_RELEASE --output "$ROOTFS_DIR"/tmp/ambilight-wifi.deb
 curl -sS -L --get https://github.com/tihoangyeudau/rpi-fan/releases/download/1.0.0/rpi-fan.tar.gz | tar --strip-components=0 -C ${ROOTFS_DIR}/usr/share/ rpi-fan -xz
 
 # Copy service file
@@ -29,7 +29,7 @@ sed -i "s/^VERSION=.*$/VERSION=\"${HYPERION_LATEST_VERSION}\"/g" ${ROOTFS_DIR}/u
 
 on_chroot << EOF
 echo '           Install Ambilight WiFi'
-apt install /tmp/ambilightwifi.deb
+apt install /tmp/ambilight-wifi.deb
 echo '           Register Ambilight WiFi'
 systemctl -q enable ambilightwifid"@rml".service
 systemctl -q enable rpi-fan.service
